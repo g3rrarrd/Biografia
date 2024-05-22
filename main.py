8ef771925b5768052b933bf1e83cff7dc3076a09
g3rrarrd <g_rodriguez51@outlook.com>
2024-05-22 12:00:25 -0600

    se agrego main

---
+++ main.py
@@ -0,0 +1,39 @@
+import questions as q
+
+data = {
+        "Nombre": "", 
+        "Fecha de nacimiento": "",
+        "Direccion": "",
+        "Metas personales": "",
+        "Objetivo principal": ""
+    }
+
+class main:
+
+    def validate_input(question_text, validator):
+        while True:
+            print(question_text)
+            user_input = input()
+            if validator(user_input):
+                return user_input
+            else:
+                print("Caracter invalido. Intente de nuevo.")
+
+    def enter_data(key, answer):
+        data[key] = answer
+
+    question = q.questions()
+        
+    for key in data.keys():
+        if key == "Fecha de nacimiento":
+            user_input = validate_input(question.pregunta(), question.validate_date)
+        else:
+            user_input = validate_input(question.pregunta(), question.validate_text)
+            
+        enter_data(key, user_input)
+        print()
+
+    for key, value in data.items():
+        print("%s: %s" % (key, value))
+
+ 
